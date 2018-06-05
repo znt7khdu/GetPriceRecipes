f1821893bf7d29b29502f1395093bd478a45031e
znt7khdu <znt7khdu@gmail.com>
2018-06-05 20:03:54 +0900

    例外を考慮した

--- src/get_recipe.py
+++ src/get_recipe.py
@@ -7,14 +7,29 @@ def get_recipe(url):
     site=url
     hdr = {'User-Agent': 'Mozilla/5.0'}
     req = Request(site,headers=hdr)
-    page = urlopen(req)
+
+    try:
+        page = urlopen(req)
+    #HTTP Error 404の場合Noneを返す
+    except:
+        TITLE = ""
+        SERVNUM = ""
+        INGREDIENTNAME = [""]
+        INGREDIENTAMO = [""]
+        return TITLE,SERVNUM,INGREDIENTNAME,INGREDIENTAMO
+
     soup = BeautifulSoup(page, "lxml")
 
     #レシピのタイトルを取得
     title = soup.find("h1", class_="recipe-title").text
 
     #レシピの人数を取得
-    servnum = soup.find("span", class_="servings_for").text
+    servnum = soup.find("span", class_="servings_for")
+    #人数が空白の場合を考慮する
+    if servnum is None:
+        servnum = ""
+    else:
+        servnum = servnum.text
 
     name = []
     ammo = []
@@ -25,8 +40,15 @@ def get_recipe(url):
 
     #ブロックから列を一つずつ抽出し、材料と量を配列に格納する
     for row in rows:
-        name.append(row.find("span", class_="name").text)
-        ammo.append(row.find("div", class_="amount").text)
+        #空行とカテゴリ行を取得する
+        space = row.find("div", class_ = "ingredient_spacer")
+        category = row.find("div", class_ = "ingredient_category")
+
+        #空行だったりとカテゴリ行でなかった場合のみ名前と量を取得する
+        if space is None:
+            if category is None:
+                name.append(row.find("span", class_="name").text)
+                ammo.append(row.find("div", class_="amount").text)
 
     #取得した値を返り値に渡す
     TITLE = re.sub('\n', '', title, flags=re.MULTILINE)
@@ -36,12 +58,15 @@ def get_recipe(url):
 
     return TITLE,SERVNUM,INGREDIENTNAME,INGREDIENTAMO
 
-
-
 if __name__ == "__main__":
 
-    TITLE,SERVNUM,INGREDIENTNAME,INGREDIENTAMO = get_recipe("https://cookpad.com/recipe/3970570")
-    print(TITLE)
-    print(SERVNUM)
-    print(INGREDIENTNAME)
-    print(INGREDIENTAMO)
+    #とりあえず10個のレシピで検証
+    for num in range(400000,400010):
+        url = "https://cookpad.com/recipe/" + str(num)
+        TITLE,SERVNUM,INGREDIENTNAME,INGREDIENTAMO = get_recipe(url)
+
+        print(url)
+        print(TITLE)
+        print(SERVNUM)
+        print(INGREDIENTNAME)
+        print(INGREDIENTAMO)
